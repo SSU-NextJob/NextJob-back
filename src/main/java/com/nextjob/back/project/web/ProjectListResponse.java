963268e8af6d198ead6be6f3b1d4e813bf2dfa64
juhyun5060 <juhyun5060@gmail.com>
2025-07-10 21:31:22 +0900

    add: ProjectListResponse에 content, image 추가

--- src/main/java/com/nextjob/back/project/web/ProjectListResponse.java
+++ src/main/java/com/nextjob/back/project/web/ProjectListResponse.java
@@ -8,7 +8,9 @@ import java.time.LocalDateTime;
 public class ProjectListResponse {
     private int projectId;
     private String name;
+    private String content;
     private String projectType;
     private LocalDateTime startAt;
     private LocalDateTime endAt;
+    private String image;
 }

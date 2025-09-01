952de390546c51863ea56941112d8f0efb990991
silverstar03 <eunbyoul0421@gmail.com>
2025-09-01 15:42:23 +0900

    문서 API 개발 - 문서 목록 조회 API 개발 - 문서 삭제 API 개발

--- src/main/java/com/nextjob/back/drive/web/DriveController.java
+++ src/main/java/com/nextjob/back/drive/web/DriveController.java
@@ -1,10 +1,6 @@
 package com.nextjob.back.drive.web;
 
 import com.nextjob.back.drive.service.DriveService;
-import com.nextjob.back.kanbanBoard.service.KanbanService;
-import com.nextjob.back.kanbanBoard.web.KanbanSearchCriteria;
-import com.nextjob.base.exception.CustomException;
-import com.nextjob.base.exception.ErrorCode;
 import com.nextjob.base.util.CamelCaseMap;
 import com.nextjob.base.web.response.ApiResponse;
 import org.springframework.util.ObjectUtils;
@@ -38,6 +34,35 @@ public class DriveController {
 
         driveService.uploadFile(driveId, file);
 
+        return ApiResponse.ok(result);
+    }
+
+    /**
+     * 문서 목록 조회
+     *
+     * @param driveId
+     * @return
+     */
+    @GetMapping("/{driveId}/blobs")
+    public ApiResponse<List<CamelCaseMap>> findFile(@PathVariable("driveId") int driveId,
+                                                    @RequestParam(required = false) String search) {
+        List<CamelCaseMap> fileList = driveService.findFileList(driveId, search);
+        if (ObjectUtils.isEmpty(fileList)) {
+            fileList = new ArrayList<>();
+        }
+        return ApiResponse.ok(fileList);
+    }
+
+    /**
+     * 문서 삭제
+     *
+     * @param blobId
+     * @return
+     */
+    @DeleteMapping("/blobs/{blobId}")
+    public ApiResponse<Map<String, Object>> deleteFile(@PathVariable("blobId") int blobId) {
+        Map<String, Object> result = new HashMap<>();
+        result = driveService.deleteFile(blobId);
         return ApiResponse.ok(result);
     }
 

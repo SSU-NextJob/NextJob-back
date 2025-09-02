4fcbf4acb5a90ae2b9330a72003e8572caf463c5
areum0505 <areum05055@gmail.com>
2025-09-02 22:21:30 +0900

    문서 업로드 항목 추가

--- src/main/java/com/nextjob/back/drive/domain/Blob.java
+++ src/main/java/com/nextjob/back/drive/domain/Blob.java
@@ -1,11 +1,15 @@
 package com.nextjob.back.drive.domain;
 
+import com.nextjob.base.domain.Domain;
 import lombok.Data;
 
 @Data
-public class Blob {
+public class Blob extends Domain {
     private int workspaceId;
     private String blobUrl;
     private int driveId;
     private int userId;
+    private String ext;
+    private String name;
+    private long size;
 }

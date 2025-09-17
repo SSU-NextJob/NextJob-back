d748a569e4df0c6c2c95d6391e0b931bd9421e85
areum0505 <areum05055@gmail.com>
2025-09-17 22:46:23 +0900

    작업 상태 변경 (순서 및 상태)

--- src/main/java/com/nextjob/back/kanbanBoard/service/KanbanService.java
+++ src/main/java/com/nextjob/back/kanbanBoard/service/KanbanService.java
@@ -1,5 +1,6 @@
 package com.nextjob.back.kanbanBoard.service;
 
+import com.nextjob.back.kanbanBoard.domain.KanbanTasks;
 import com.nextjob.back.kanbanBoard.web.KanbanSearchCriteria;
 import com.nextjob.base.util.CamelCaseMap;
 
@@ -19,4 +20,6 @@ public interface KanbanService {
     Map<String, Object> findTaskDetail(int kanbanId, int taskId);
 
     List<CamelCaseMap> findColumnList(int kanbanId);
+
+    int updateTaskStatus(List<KanbanTasks> tasks);
 }
